a25b4cb85a80f734c43e65c27c27410e6b7ce0c0
rapturt9 <ram.potham@gmail.com>
2021-02-02 20:20:10 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import serial from './serial.js';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let port = null;
+
+class App extends Component {
+
+  state={val:"", connect: false}
+
+  componentDidMount () {
+    serial.getPorts().then(ports => {
+      if (ports.length === 0) {
+        console.log('No device found.');
+      } else {
+        console.log('Connecting...');
+        port = ports[0];
+        this.connect();
+      }
+    });
+  }
+
+  handleChange = e =>{
+    this.setState({val:e.target.value});
+  }
+
+  keyPressed = e => {
+    if (e.key === "Enter") {
+      let view = new Uint8Array(1);
+      view[0] = parseInt(this.state.val);
+      port.send(view);
+    }
+  }
+
+  connect = () => {
+    port.connect().then(() => {
+        this.setState({connect: true});
+        port.onReceive = data => {
+          let textDecoder = new TextDecoder();
+          console.log(textDecoder.decode(data));
+        }
+        port.onReceiveError = error => {
+          console.error(error);
+        };
+      }, error => {
+        console.log(error);
+      });
+  }
+
+  connectdevice = () => {
+    console.log("connecting");
+    if (port) {
+      port.disconnect();
+      port = null;
+      this.setState({connect: false});
+    } else {
+      serial.requestPort().then(selectedPort => {
+        port = selectedPort;
+        console.log(port);
+        this.connect();
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }
+  
+  render (){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick = {this.connectdevice}>{this.state.connect ? 'Connected':'Connect'}</button>
+          <input onChange={this.handleChange} onKeyPress={this.keyPressed} value={this.state.val} />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
